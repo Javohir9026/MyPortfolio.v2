0a12ddc1901631a28c8ebb1293a8d74c47224228
Javohir9026 <javohiramanbayev9026@gmail.com>
2026-05-07 16:57:44 +0500

    updated

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,10 @@ export interface Project {
   description: string;
   tech: string[];
   github: string;
+  githubAccess: "public" | "private";
+  InProgress: boolean;
   live: string;
+  image?: string;
   featured: boolean;
 }
 
@@ -12,41 +15,80 @@ export const projects: Project[] = [
   {
     id: 1,
     title: "Nasiya Savdo - Admin Panel",
+    image: "/NasiyaSavdoAdminka.png",
     description:
       "React + TypeScript da yozilgan zamonaviy admin paneli. Shadcn UIdan komponentlar, Backend Api To'liq CRUD, Lucide-react iconlardan foydalanilgan. To'liq responsive va foydalanuvchi uchun qulay interfeysga ega. Loyihani Storybook yordamida hujjatlashtirish va test qilish rejalashtirilgan.",
-    tech: ["React", "TypeScript", "Shadcn UI", "Tailwind CSS", "Lucide-React", "CSS Variables"],
+    tech: [
+      "React",
+      "TypeScript",
+      "Shadcn UI",
+      "Tailwind CSS",
+      "Lucide-React",
+      "CSS Variables",
+    ],
     github: "https://github.com/Javohir9026/nasiya-savdo-front-end",
+    githubAccess: "public",
+    InProgress: false,
     live: "https://nasiya-savdo-admin-panel.vercel.app/",
     featured: true,
   },
   {
     id: 2,
-    title: "Nasiya Savdo – landing page",
+    title: "Nasiya Savdo - landing page",
+    image: "/NasiyaSavdoLanding.png",
     description:
       "React + TypeScript da yozilgan zamonaviy landing page. Shadcn UIdan komponentlar, Lucide-react iconlardan foydalanilgan. To'liq responsive va foydalanuvchi uchun qulay interfeysga ega.",
-    tech: ["React", "TypeScript", "Tailwind CSS", "Shadcn UI", "Lucide-React", "CSS Variables"],
+    tech: [
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Shadcn UI",
+      "Lucide-React",
+      "CSS Variables",
+    ],
     github: "https://github.com/Javohir9026/NasiyaLandingPage",
+    githubAccess: "public",
+    InProgress: false,
     live: "https://nasiya-landing-page-qrzr.vercel.app/",
     featured: true,
   },
   {
     id: 3,
-    title: "Hotel Luxora",
+    title: "Yasin Mebel - Enterprise ERP",
+    image: "/YasinMebel.png",
     description:
-      "Hotel Luxora — zamonaviy va responsiv mehmonxona web-ilovasi. Foydalanuvchilar uchun xonalarni ko'rish, bron qilish va xizmatlar bilan tanishish imkoniyatini taqdim etadi. UI/UX, tezlik va qulaylikka alohida e’tibor qaratilgan.",
-    tech: ["HTML", "CSS", "UI/UX", "Responsive Design"],
-    github: "https://github.com/Javohir9026/hotel-luxora",
-    live: "https://hotel-luxora-nine.vercel.app/",
+      "Yasin Mebel uchun ishlab chiqilgan to'liq ERP tizimi. React + TypeScript da yozilgan, shadcn UI dan komponentlar, Lucide-react iconlardan foydalanilgan. To'liq responsive va foydalanuvchi uchun qulay interfeysga ega. Loyihani Storybook yordamida hujjatlashtirish va test qilish rejalashtirilgan.",
+    tech: [
+      "React",
+      "TypeScript",
+      "Shadcn UI",
+      "Tailwind CSS",
+      "Lucide-React",
+      "State Management",
+    ],
+    github: "",
+    githubAccess: "private",
+    InProgress: true,
+    live: "https://yasinmebel.uz/",
     featured: true,
   },
   {
     id: 4,
-    title: "PlayShoke – CS2 Training servers",
+    title: "Pul Hisob - Business Management System",
+    image: "/PulHisob.png",
     description:
-      "PlayShoke - CS2 o'yinini o'ynash va mashq qilish uchun mo'ljallangan trening serverlari. Foydalanuvchilar uchun qulay interfeys, tezkor javob berish va o'yin ichidagi tajribani yaxshilashga qaratilgan. Loyiha Hozircha Backendga to'liq ulanmagan va faqat frontend qismi mavjud. Keyinchalik Real Loyiha qilish rejalashtirilgan.",
-    tech: ["Next.js", "TypeScript", "MockData", "CSS Modules", "Tailwind CSS"],
-    github: "https://github.com/Javohir9026/PlayShoke",
-    live: "https://play-shoke-gules.vercel.app/",
-    featured: false,
+      "Pul Hisob uchun ishlab chiqilgan biznes boshqaruv tizimi. React + TypeScript da yozilgan Ui Kutubxonalardan foydalanilgan. To'liq responsive va foydalanuvchi uchun qulay interfeysga ega.",
+    tech: [
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "State Management",
+      "Ui Libraries",
+    ],
+    github: "",
+    githubAccess: "private",
+    InProgress: false,
+    live: "https://pulhisob.uz/",
+    featured: true,
   },
 ];
